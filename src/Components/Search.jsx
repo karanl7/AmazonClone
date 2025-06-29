57b3585ea719205288c5b18797a95ab4bed1cd65
karan <karannisar811@gmail.com>
2025-06-29 16:07:35 +0530

    changes

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -3,8 +3,8 @@ import {MagnifyingGlassIcon} from '@heroicons/react/24/outline'
 const Search = () => {
   return (
     <div className="w-[100%]">
-        <div className="flex items-center h-9.5 bg-amazonclone-yellow rounded">
-            <select className="p-2 bg-gray-300 text-black border text-sm xl:text-m ">
+        <div className="flex items-center h-9.5 bg-amazonclone-yellow border-amazonclone">
+            <select className="p-2 bg-gray-300 text-black border-2  text-sm xl:text-m ">
                 <option>All</option>
                 <option>Deals</option>
                 <option>Amazon</option>
@@ -13,10 +13,11 @@ const Search = () => {
                 <option>Home</option>
                 <option>Mobiles</option>
             </select>
-            <input className="flex grow items-center h-[100%] rounded-l text-black border bg-amazonclone-background  " type='text'/>
-            <button className="w-[45px]">
+            <input className="flex grow  items-center h-[100%] text-black  bg-amazonclone-background " type='text'/>
+            <button className="w-[45px] rounded-r border-amazonclone ">
                 <MagnifyingGlassIcon className="h-[27px] m-auto stroke-slate-900"/>
             </button>
+
         </div>
     </div>
   )
